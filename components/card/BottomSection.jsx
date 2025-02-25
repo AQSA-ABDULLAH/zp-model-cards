4a92bafc638b8c0be5ea5c4b9b16cf92632518e3
Aqsa-Abdullah090 <aqsa.abdull@zimogroup.org>
2025-02-25 12:25:42 +0500

    fix styling

--- components/card/BottomSection.jsx
+++ components/card/BottomSection.jsx
@@ -10,14 +10,14 @@ export default function BottomSection() {
       <Timer />
 
       {/* Location */}
-      <div className="text-[6px] 3xl:text-[8px] flex justify-center items-center gap-2 mt-[16px]">
+      <div className="text-[6px] 3xl:text-[8px] flex justify-center items-center gap-2 mt-[16px] tracking-[1px]">
         <p>London</p>
         <p>SW1A</p>
         <p>United Kingdom</p>
       </div>
 
       {/* Price */}
-      <p className="text-[10.7px] 3xl:text-[14px] text-center my-[10px] 3xl:my-[16px]">
+      <p className="text-[10.7px] 3xl:text-[14px] text-center my-[10px] 3xl:my-[16px] tracking-[1px]">
         <span dir="ltr">£</span>1,627,271 GBP
       </p>
 
@@ -25,7 +25,7 @@ export default function BottomSection() {
       <div className="w-[75%] mx-auto flex flex-col gap-2.5 3xl:gap-4">
         <div className="h-[1px] 3xl:h-[2px] bg-current"></div>
 
-        <button className="text-[10.7px] 3xl:text-[14px] border border-white py-[8px] flex justify-evenly transition-all duration-300 hover:bg-white hover:text-black">
+        <button className="text-[10.7px] 3xl:text-[14px] border border-white py-[8px] flex justify-evenly transition-all duration-300 hover:bg-white hover:text-black tracking-[1.2px]">
           <p dir="ltr">
             <span dir="ltr">£</span>25.00 GBP
           </p>
@@ -45,7 +45,7 @@ export default function BottomSection() {
 
       {/* Footer */}
       <footer className="px-[12px] 3xl:px-[20px] mt-auto pb-[12px] 3xl:pb-[20px] flex justify-between items-end">
-        <p className="text-[7px] 3xl:text-[9px]">#ZM78612345622</p>
+        <p className="text-[7px] 3xl:text-[9px] tracking-[1px]">#ZM78612345622</p>
         <img
           src="https://zimopro.com/assets/temp/agents_logos/Savills.svg"
           alt="Savills Logo"
